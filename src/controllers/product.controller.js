a1ead97905ef3bf951322bb1fee70bb275c2c7d1
HardikThummar97 <hardikthummar97@gmail.com>
2021-11-11 18:01:08 +0530

    make it better

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,21 +1,21 @@
-const express = require("express");
+const express = require( "express" );
 const router = express.Router();
 
-const Product = require("../models/product.model");
+const Product = require( "../models/product.model" );
 
-router.get("/:id", async (req, res) => {
-  const product = await Product.findById(req.params.id).lean().exec();
-  res.send(product);
-});
+router.get( "/:id", async ( req, res ) => {
+  const product = await Product.findById( req.params.id ).lean().exec();
+  res.send( product );
+} );
 
-router.get("/", async (req, res) => {
+router.get( "/", async ( req, res ) => {
   const products = await Product.find().lean().exec();
-  res.send(products);
-});
+  res.send( products );
+} );
 
-router.post("/", async (req, res) => {
-  const products = await Product.create(req.body);
-  res.send({ Added: products });
-});
+router.post( "/", async ( req, res ) => {
+  const products = await Product.create( req.body );
+  res.send( { Added: products } );
+} );
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,30 +1,30 @@
 //Import express & path;
-const path = require("path");
-const express = require("express");
+const path = require( "path" );
+const express = require( "express" );
 const app = express();
-app.use(express.json());
+app.use( express.json() );
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-app.use("/static", express.static(path.join(__dirname, "public")));
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
+app.use( "/static", express.static( path.join( __dirname, "public" ) ) );
 //Import connection;
-const connect = require("./configs/db");
+const connect = require( "./configs/db" );
 
 //Import routers;
-const homeController = require("./controllers/home.controller");
-app.use("/home", homeController);
+const homeController = require( "./controllers/home.controller" );
+app.use( "/home", homeController );
 
-const productController = require("./controllers/product.controller");
-app.use("/products", productController);
+const productController = require( "./controllers/product.controller" );
+app.use( "/products", productController );
 
-const cartController = require("./controllers/cart.controller");
-app.use("/cart", cartController);
+const cartController = require( "./controllers/cart.controller" );
+app.use( "/cart", cartController );
 
-const userController = require("./controllers/user.controller");
-app.use("/users", userController);
+const userController = require( "./controllers/user.controller" );
+app.use( "/users", userController );
 
 //Start connection;
-app.listen(3000, async () => {
+app.listen( 3000, async () => {
   await connect();
-  console.log("Listening on port 3000");
-});
+  console.log( "Listening on port 3000" );
+} );
