587c0b6554751868c960f8c0edee22e057f7f08c
HardikThummar97 <hardikthummar97@gmail.com>
2021-11-11 19:22:35 +0530

    Changes for deployment

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require( "express" );
 const app = express();
 app.use( express.json() );
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.set( "views", path.join( __dirname, "views" ) );
 app.set( "view engine", "ejs" );
