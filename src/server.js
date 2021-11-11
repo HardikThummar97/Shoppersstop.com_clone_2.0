8c31c85d847475ba25af01aef588c4dbbdbcabf9
HardikThummar97 <hardikthummar97@gmail.com>
2021-11-11 18:21:24 +0530

    make it better

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const connect = require( "./configs/db" );
 
 //Import routers;
 const homeController = require( "./controllers/home.controller" );
-app.use( "/home", homeController );
+app.use( "/", homeController );
 
 const productController = require( "./controllers/product.controller" );
 app.use( "/products", productController );
